f508bcb5e572f120feffe32598429e30e2fb3fbe
Abiye <abiye@MacBook-Pro.local>
2025-09-20 16:09:19 +0100

    Initial commit

---
+++ src/test/java/org/example/mapstructexercise/MapStructExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mapstructexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
